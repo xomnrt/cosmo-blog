5a4d3212d5498648d057a2b1af4c517b9a9b1fe2
xomnrt <xomnrt1@ya.ru>
2024-05-03 19:10:57 +0300

    project init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cosmo-blog
+
 NASA news interface
+[![Maintainability](https://api.codeclimate.com/v1/badges/b34f1224417e4586f99c/maintainability)](https://codeclimate.com/github/xomnrt/cosmo-blog/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new miniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
